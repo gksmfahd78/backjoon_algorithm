438fc662633afe31cc1a8f77b1df8918a957bd4b
gksmfahd78 <54091411+gksmfahd78@users.noreply.github.com>
2020-02-10 15:03:52 +0900

    Update main.py

--- baekjoon_4673/main.py
+++ baekjoon_4673/main.py
@@ -4,7 +4,6 @@ generated_number_set = set()
 for i in range(1, 10001):
     for j in str(i):
         i += int(j)
-    #print('num = ' + str(i))
     generated_number_set.add(i)
 
 self_numbers_set = natural_number_set - generated_number_set
