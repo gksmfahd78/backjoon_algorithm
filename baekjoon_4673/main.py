ef16b44a99f7e6c092d044c6d1baba80f8299198
gksmfahd78 <54091411+gksmfahd78@users.noreply.github.com>
2020-03-04 10:55:42 +0900

    Update main.py

--- baekjoon_4673/main.py
+++ baekjoon_4673/main.py
@@ -1,17 +1,9 @@
-natural_number_set = set(range(1, 10001)) #natural_number_set 크기를 10000으로 잡는다. 
-generated_number_set = set() #generated_number_set는 정수를 넣는 변수
+natural_number_set = set(range(1, 10001))
+generated_number_set = set()
 
 for i in range(1, 10001):
-    #print(' i = ' + str(i))
     for j in str(i):
-        #print('j = ' + str(j))
         i += int(j)
-    generated_number_set.add(i)
-    #print()
-
-# print(generated_number_set)
-# print(natural_number_set)
-
 self_numbers_set = natural_number_set - generated_number_set
 
 for i in sorted(self_numbers_set):
