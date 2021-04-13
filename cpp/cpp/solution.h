a727772815b32e4ea561d347347132d79cdb9847
gksmfahd78 <dlehdgus0413@gmail.com>
2021-04-13 15:42:25 +0900

    문제 업데이트

--- cpp/cpp/solution.h
+++ cpp/cpp/solution.h
@@ -1,16 +1,25 @@
 #include "bits-stdc++.h"
 
 //error
-#pragma warning(disable: 4996)
+#pragma warning(disable: 4996) //scanf ¿¡·¯
 
 //1000
 int solution_1000();
+
 int solution_1001();
+
 int solution_1002();
+
 int solution_1003();
 long long fibo(int N);
+
 int solution_1004();
+
 int solution_1005();
 int DFS(int v);
+
 int solution_1006();
 int covered(int idx, int prev, int last);
+
+int solution_1007();
+void combination(int N, int R, int q);
